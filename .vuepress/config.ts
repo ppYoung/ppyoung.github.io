c149cbbda2c7ee1e46133eb6081c6521b8271e29
dongyang.luo <dongyang.luo@shopee.com>
2023-03-22 16:39:45 +0800

    feat: 博客初始化以及更新第一篇文章

---
+++ .vuepress/config.ts
@@ -0,0 +1,91 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+
+export default defineUserConfig({
+  title: "ppYoung",
+  description: "Just playing around",
+  head: [["link", { rel: "icon", href: "/logo.jpg" }]],
+  theme: recoTheme({
+    style: "@vuepress-reco/style-default",
+    logo: "/logo.jpg",
+    author: "ppYoung",
+    authorAvatar: "/logo.jpg",
+    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {},
+    navbar: [
+      { text: "Home", link: "/" },
+      { text: "Categories", link: "/categories/tech/1/" },
+      { text: "Tags", link: "/tags/chatGPT/1/" },
+    ],
+    bulletin: {
+      // body: [
+      //   {
+      //     type: "text",
+      //     content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
+      //     style: "font-size: 12px;",
+      //   },
+      //   {
+      //     type: "hr",
+      //   },
+      //   {
+      //     type: "title",
+      //     content: "QQ 群",
+      //   },
+      //   {
+      //     type: "text",
+      //     content: `
+      //     <ul>
+      //       <li>QQ群1：1037296104</li>
+      //       <li>QQ群2：1061561395</li>
+      //       <li>QQ群3：962687802</li>
+      //     </ul>`,
+      //     style: "font-size: 12px;",
+      //   },
+      //   {
+      //     type: "hr",
+      //   },
+      //   {
+      //     type: "title",
+      //     content: "GitHub",
+      //   },
+      //   {
+      //     type: "text",
+      //     content: `
+      //     <ul>
+      //       <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+      //       <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+      //     </ul>`,
+      //     style: "font-size: 12px;",
+      //   },
+      //   {
+      //     type: "hr",
+      //   },
+      //   {
+      //     type: "buttongroup",
+      //     children: [
+      //       {
+      //         text: "打赏",
+      //         link: "/docs/others/donate.html",
+      //       },
+      //     ],
+      //   },
+      // ],
+    },
+    // valineConfig 配置与 1.x 一致
+    // valineConfig: {
+    //   appId: 'xxx',
+    //   appKey: 'xxx',
+    //   placeholder: '填写邮箱可以收到回复提醒哦！',
+    //   verify: true, // 验证码服务
+    //   // notify: true,
+    //   recordIP: true,
+    //   // hideComments: true // 隐藏评论
+    // },
+  }),
+  // debug: true,
+});
---
+++ README.md
@@ -0,0 +1,11 @@
+---
+home: true
+modules:
+  - banner
+  - blog
+banner:
+  heroText: ppYoung
+  tagline: 近朱者赤，近我者甜
+  heroImage: "/logo.jpg"
+  bgImage: "/bg.svg"
+---
